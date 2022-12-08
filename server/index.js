486926bc52c87b3352d719e7d7a3366520fcb600
lseeker5 <118029799+lseeker5@users.noreply.github.com>
2022-12-09 00:25:57 +0800

    yes

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app=express()
+const mysql=require('mysql')
 
+const db=mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password:'4yYZGBBTmR5!',
+    database:'crud_trial'
+})
 
 app.get('/',(req,res)=>{
-    res.send("Helloworld")
+    const sqlInsert="INSERT INTO new_table(movie_name,movie_review) VALUES ('gayfuck','so gay');"
+    db.query(sqlInsert,(err,result)=>{res.send("Helloworld")})
+    
 })
 
 app.listen(3001,()=>{
     console.log("Server is running")
 })
+
+
